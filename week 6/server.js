37bd74fe73f6553ea91d7ab803b6cc428ae2c3a3
JadeMuyambo <muyambojade@gmail.com>
2026-02-17 17:46:48 -0500

    week 6

---
+++ week 6/server.js
@@ -0,0 +1,83 @@
+//1. setup a node app with command: npm init
+//2. install express with command: npm install express
+//3. create a file named server.js amd add the following code
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//https://www.npmjs.com/package/express-handlebars is a Handlebarsview engine for Express which provides a way to render dynamic HTML
+const hbs = require("express-handlebars");
+
+app.engine ("handlebars", hbs.engine());
+app.set("view engine", "handlebars");
+//app.set("views", path.join(__dirname, "views"));
+//the path module is used to work with file and directory paths
+const path = require("path");
+
+//serving static files
+// express.static is a built-in middleware function in Express. It serves static files and is based on server-static.
+//The function takes a root directory
+app.use(express.static(path.join(__dirname, "static")));
+
+// data
+const directory = require("./data/directory.json");
+console.log(directory);
+
+// generate routes
+app.get('/',(req, res) => {
+    // sendfile is used to send a file as a response
+    let filePath = path.join(__dirname, "static", "homepage.html");
+    res.sendFile(filePath);
+});
+
+// rendering templates
+app.get("/home", (req, res) => {
+    res.render("home", {title: "My Website's homepage"});
+ });
+
+app.get("/about",(req, res) => {
+    // sendfile is used to send a file as a response
+    let filePath = path.join(__dirname, "static", "about.html");
+    res.sendFile(filePath);
+});
+
+app.get("/images/sample.jpg", (req, res) => {
+let filePath = path.join(__dirname, "static", "images", "sample.jpg");   
+res.sendFile(filePath); 
+});
+
+//HTTP METHODS GET POST PUT DELETE
+//GET
+app.get("/api/items",(req, res) => {
+    res.send("this is a get response from /api/items");
+});
+//POST
+app.post("/api/items",(req, res) => {
+    res.send("this is a post response from /api/items");
+});
+//PUT
+app.put("/api/items/:id",(req, res) => {
+    res.send("this is a put response from /api/items");
+});
+//DELETE
+app.delete("/api/items/:id",(req, res) => {
+    res.send("this is a delete response from /api/items");
+});
+
+//Directory route
+app.get("/directory", (req, res) => {
+    res.render ("directory", {people:
+    directory });
+});
+
+app.get("/directory/:id", (req, res) => {
+    //TODO: find the person ith the id in the directory  and pass it to the template
+    res.render ("directory", {people:
+    directory });
+});
+
+// start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
